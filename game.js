b797a4e7e5f988b21988520c8e8f26e268fc4cb0
JimNewaz <miznewaz@gmail.com>
2020-11-11 17:31:44 +0600

    last commit

---
+++ game.js
@@ -0,0 +1,21 @@
+function theyfire1(){   
+    
+         HIT6.load();
+          HIT6.play();
+    var get1 = document.querySelector("#they1");
+     get1.src="images/boycott.png";
+ 
+    setTimeout(removd,1500);  
+    
+}
+
+ function theyfire2(){    
+    
+         HIT6.load();
+          HIT6.play();
+    var get2 = document.querySelector("#they2");
+     get2.src="images/boycott.png";
+ 
+    setTimeout(removd2,1500);
+    
+}
